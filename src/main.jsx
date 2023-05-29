583674c135316a963b4b3820a5b527e84abb61f1
jjribeiro1 <jvsr.99@hotmail.com>
2023-05-29 14:03:54 -0300

    "fix: main file"

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <App />
     </ThemeProvider>
-    <App />
   </React.StrictMode>
 );
