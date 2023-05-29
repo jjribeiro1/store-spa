c926088fb39588bf67eace15e239315ec197f15b
jjribeiro1 <jvsr.99@hotmail.com>
2023-05-28 22:53:05 -0300

    "chore: add prettier"

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,7 @@ export default {
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
+    "prettier",
   ],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
