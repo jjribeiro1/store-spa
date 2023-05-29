394e96f13e00813b56e45e272f0d0279c4daed67
jjribeiro1 <jvsr.99@hotmail.com>
2023-05-29 13:56:23 -0300

    "fix: import ThemeProvider"

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { ThemeProvider } from "@mui/styles";
+import { ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import { theme } from "./theme.js";
 
