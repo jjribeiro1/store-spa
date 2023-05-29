a7df0b0c5e77c5f4afabae293c751c5b0739675f
jjribeiro1 <jvsr.99@hotmail.com>
2023-05-29 14:39:01 -0300

    "feat: routes setup and folder architecture"

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Home from "./scenes/home/Home";
+import ItemDetails from "./scenes/itemDetails/itemDetails";
+import Checkout from "./scenes/checkout/Checkout";
+import Confirmation from "./scenes/checkout/Confirmation";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
 function App() {
   return (
     <>
-      <div className="app">app</div>
+      <div className="app">
+        <BrowserRouter>
+          <ScrollToTop />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/item/:id" element={<ItemDetails />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/checkout/success" element={<Confirmation />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </>
   );
 }
