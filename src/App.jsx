e64cf1969b67d3c5e4adc7793f2dab14acae603f
jjribeiro1 <jvsr.99@hotmail.com>
2023-05-30 18:20:11 -0300

    "feat: add Footer and Subscribe component"

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import Checkout from "./scenes/checkout/Checkout";
 import Confirmation from "./scenes/checkout/Confirmation";
 import Home from "./scenes/home/Home";
-import ItemDetails from "./scenes/itemDetails/itemDetails";
+import ItemDetails from "./scenes/itemDetails/ItemDetails";
 import NavBar from "./scenes/global/NavBar";
 import CartMenu from "./scenes/global/CartMenu";
+import Footer from "./scenes/global/Footer";
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
@@ -30,7 +31,8 @@ function App() {
             <Route path="/checkout" element={<Checkout />} />
             <Route path="/checkout/success" element={<Confirmation />} />
           </Routes>
-          <CartMenu/>
+          <CartMenu />
+          <Footer />
         </BrowserRouter>
       </div>
     </>
