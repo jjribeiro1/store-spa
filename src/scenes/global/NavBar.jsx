7f79ced5b808e5865e53a0e6499c211ffdd8432e
jjribeiro1 <jvsr.99@hotmail.com>
2023-05-30 18:33:19 -0300

    "refactor: NavBar width"

--- src/scenes/global/NavBar.jsx
+++ src/scenes/global/NavBar.jsx
@@ -22,7 +22,7 @@ const NavBar = () => {
       alignItems="center"
       width="100%"
       height="60px"
-      bgcolor="rgba(255, 255, 255, 0.95)"
+      backgroundColor="rgba(255, 255, 255, 0.95)"
       color="black"
       position="fixed"
       top="0"
@@ -30,7 +30,7 @@ const NavBar = () => {
       zIndex="1"
     >
       <Box
-        width="100%"
+        width="80%"
         margin="auto"
         display="flex"
         justifyContent="space-between"
@@ -43,16 +43,13 @@ const NavBar = () => {
         >
           ECOMMER
         </Box>
-
         <Box display="flex" justifyContent="space-between" columnGap="20px" zIndex="2">
           <IconButton sx={{ color: "black" }}>
             <SearchOutlined />
           </IconButton>
-
           <IconButton sx={{ color: "black" }}>
             <PersonOutline />
           </IconButton>
-
           <Badge
             badgeContent={cart.length}
             color="secondary"
@@ -71,7 +68,6 @@ const NavBar = () => {
               <ShoppingBagOutlined />
             </IconButton>
           </Badge>
-
           <IconButton sx={{ color: "black" }}>
             <MenuOutlined />
           </IconButton>
