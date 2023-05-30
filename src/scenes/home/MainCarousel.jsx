0120bdca5595513e6b17625f9833e3f983b247e0
jjribeiro1 <jvsr.99@hotmail.com>
2023-05-30 15:31:40 -0300

    "feat: add MainCarousel component"

---
+++ src/scenes/home/MainCarousel.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import { Box, Typography, IconButton, useMediaQuery } from "@mui/material";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import { shades } from "../../theme";
+import Slide1 from "../../assets/slide1.jpeg";
+import Slide2 from "../../assets/slide2.jpeg";
+import Slide3 from "../../assets/slide3.jpeg";
+import Slide4 from "../../assets/slide4.jpeg";
+import Slide5 from "../../assets/slide5.jpeg";
+
+const MainCarousel = () => {
+  const isNonMobile = useMediaQuery("(min-width:600px)");
+  const slides = [Slide1, Slide2, Slide3, Slide4, Slide5];
+
+  return (
+    <Carousel
+      infiniteLoop={true}
+      showThumbs={false}
+      showIndicators={false}
+      showStatus={false}
+      renderArrowPrev={(onClickHandler, hasPrev, label) => (
+        <IconButton
+          onClick={onClickHandler}
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "0",
+            color: "white",
+            padding: "5px",
+            zIndex: "10",
+          }}
+        >
+          <NavigateBeforeIcon sx={{ fontSize: 40 }} />
+        </IconButton>
+      )}
+      renderArrowNext={(onClickHandler, hasNext, label) => (
+        <IconButton
+          onClick={onClickHandler}
+          sx={{
+            position: "absolute",
+            top: "50%",
+            right: "0",
+            color: "white",
+            padding: "5px",
+            zIndex: "10",
+          }}
+        >
+          <NavigateNextIcon sx={{ fontSize: 40 }} />
+        </IconButton>
+      )}
+    >
+      {slides.map((slide, index) => (
+        <Box key={`carousel-image-${index}`}>
+          <img
+            src={slide}
+            alt={`carousel-${index}`}
+            style={{
+              width: "100%",
+              height: "700px",
+              objectFit: "cover",
+              backgroundAttachment: "fixed",
+            }}
+          />
+          <Box
+            color="white"
+            padding="20px"
+            borderRadius="1px"
+            textAlign="left"
+            backgroundColor="rgb(0, 0, 0, 0.4)"
+            position="absolute"
+            top="46%"
+            left={isNonMobile ? "10%" : "0"}
+            right={isNonMobile ? undefined : "0"}
+            margin={isNonMobile ? undefined : "0 auto"}
+            maxWidth={isNonMobile ? undefined : "240px"}
+          >
+            <Typography color={shades.secondary[200]}>-- NEW ITEMS</Typography>
+            <Typography variant="h1">Summer Sale</Typography>
+            <Typography
+              fontWeight="bold"
+              color={shades.secondary[300]}
+              sx={{ textDecoration: "underline" }}
+            >
+              Discover More
+            </Typography>
+          </Box>
+        </Box>
+      ))}
+    </Carousel>
+  );
+};
+
+export default MainCarousel;
